eebb9f76e4b74e0347e3164ba35c2cd26999d367
pavlojs <pavlojs@gmail.com>
2026-03-11 13:52:26 +0100

    feat: add scroll to top functionality on route change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Home from './pages/Home.tsx';
 import Vault from './pages/Vault.tsx';
 import Admin from './pages/Admin.tsx';
 import Tos from './pages/Tos.tsx';
 import Footer from './components/Footer.tsx';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <main className="flex-1">
         <Routes>
           <Route path="/" element={<Home />} />
